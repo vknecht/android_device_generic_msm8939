90551430bee0d19ef8e41e68ccd3d31c18f575bb
Vincent Knecht <vincent.knecht@mailoo.org>
2023-01-17 19:04:38 +0100

    build: kernel: Bundle only current device's DTB
    Don't bundle all available DTBs, only the one for the current device.
    Especially since qcom,msm-id & qcom,board-id are not mentioned anymore
    in devices' DTS files.
    
    Fixes device rebooting directly into lk2nd.
    
    Signed-off-by: Vincent Knecht <vincent.knecht@mailoo.org>

--- build/tasks/kernel.mk
+++ build/tasks/kernel.mk
@@ -1,7 +1,7 @@
 ifneq ($(filter idol3 kiwi, $(TARGET_DEVICE)),)
 
 IMAGE_GZ := device/generic/msm8939/shared/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/Image.gz
-DTB := $(wildcard device/generic/msm8939/shared/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/*.dtb)
+DTB := $(wildcard device/generic/msm8939/shared/prebuilt-kernel/android-$(TARGET_KERNEL_USE)/msm8939-*-$(TARGET_DEVICE).dtb)
 
 $(PRODUCT_OUT)/kernel: $(IMAGE_GZ) $(DTB)
 	cat $(IMAGE_GZ) $(DTB) > $@
